0ccac9e2a092432cecf073996627da0f74711ef7
NLAM1 <nicolas.lamanna@wanadoo.fr>
2018-09-24 15:14:12 +0200

    Upload

---
+++ src/main/java/com/ckoipapa/com/ckoipapa/web/UploadRestService.java
@@ -0,0 +1,61 @@
+package com.ckoipapa.com.ckoipapa.web;
+
+import com.ckoipapa.com.ckoipapa.dao.ClientRepository;
+import com.ckoipapa.com.ckoipapa.entities.Client;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+public class UploadRestService {
+
+    private static String UPLOADED_FOLDER = "C://Users//nlam1//Desktop//temp//";
+
+    @PostMapping("/api/upload")
+    //@ResponseBody
+    public ResponseEntity<?> uploadFile(
+            @RequestParam("file") MultipartFile uploadfile) {
+
+        if (uploadfile.isEmpty()) {
+            return new ResponseEntity("please select a file!", HttpStatus.OK);
+        }
+        try {
+            saveUploadedFiles(Arrays.asList(uploadfile));
+        } catch (IOException e) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity("Successfully uploaded - " +
+                uploadfile.getOriginalFilename(), new HttpHeaders(), HttpStatus.OK);
+
+    }
+
+    private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
+
+        for (MultipartFile file : files) {
+
+            if (file.isEmpty()) {
+                continue; //next pls
+            }
+
+            byte[] bytes = file.getBytes();
+            Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+            Files.write(path, bytes);
+
+        }
+
+    }
+
+
+}
